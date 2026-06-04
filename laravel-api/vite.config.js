2ae3216fc671c6e8fe420ffd0d9880c1964d5697
Ostian Stephane <ostianstephane03@gmail.com>
2026-06-04 22:56:38 +0100

    feat(docker-compose): add authentication process  and Optimize and customize the Laravel image for dev/prod environments and add a multi-stage build to the Dockerfile

--- laravel-api/vite.config.js
+++ laravel-api/vite.config.js
@@ -1,24 +1,28 @@
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
-import { bunny } from 'laravel-vite-plugin/fonts';
-import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
-    plugins: [
-        laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js'],
-            refresh: true,
-            fonts: [
-                bunny('Instrument Sans', {
-                    weights: [400, 500, 600],
-                }),
-            ],
-        }),
-        tailwindcss(),
-    ],
     server: {
+        host: true,
+        port: 5173,
+
+        hmr: {
+            host: 'localhost',
+            port: 5173,
+        },
+
         watch: {
-            ignored: ['**/storage/framework/views/**'],
+            usePolling: true,
         },
     },
+
+    plugins: [
+        laravel({
+            input: [
+                'resources/sass/app.scss',
+                'resources/js/app.js',
+            ],
+            refresh: true,
+        }),
+    ],
 });
