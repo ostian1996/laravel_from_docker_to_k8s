5c02791f815f6ea801776a30e632b39928415054
Ostian Stephane <ostianstephane03@gmail.com>
2026-06-06 23:39:05 +0100

    feat(dashboard) Setting up dashboard view and add roles é user features in progress

--- laravel-api/routes/web.php
+++ laravel-api/routes/web.php
@@ -1,11 +1,22 @@
 <?php
 
+use App\Http\Controllers\DashboardController;
+use App\Http\Controllers\RoleController;
+use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', fn() => redirect()->route('login'));
 
 Auth::routes();
 
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+// Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+// Authenticated routes
+Route::middleware('auth')->group(function () {
+    // Dashboard
+    Route::get('dashboard', [DashboardController::class, 'index'])->name('home');
+
+    // Roles and Users
+    Route::resource('roles', RoleController::class);
+    Route::resource('users', UserController::class)->except('show');
+});
