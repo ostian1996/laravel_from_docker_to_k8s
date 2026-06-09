c4a26253c1d508e0b79a1bb3c361706999937518
Ostian Stephane <ostianstephane03@gmail.com>
2026-06-09 23:09:42 +0100

    fix: skip the middleware dns verification if you're in a test environment

--- laravel-api/app/Http/Middleware/EnsureApiDomain.php
+++ laravel-api/app/Http/Middleware/EnsureApiDomain.php
@@ -17,6 +17,10 @@ class EnsureApiDomain
     {
         $host = $request->getHost(); // "api.local" ou "admin.local"
 
+        if (app()->environment('testing')) {
+            return $next($request);
+        }
+
         // Si la requête arrive sur api.local → on laisse passer
         // Si elle arrive ailleurs → 404
         if ($host !== 'api.local') {
